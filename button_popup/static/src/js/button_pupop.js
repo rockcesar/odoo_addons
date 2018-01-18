f699cc416be066f4bb9252f3bd116ad1b4d55ee5
César Cordero <cesar.cordero.r@gmail.com>
2018-01-18 19:59:14 -0400

    New module Button Popup foir all Forms.

---
+++ button_popup/static/src/js/button_pupop.js
@@ -0,0 +1,89 @@
+odoo.define('button_popup.FormView', function (require) {
+"use strict";
+    
+    //var instance = openerp;
+    
+    var FormView = require('web.FormView');
+    
+    var core = require('web.core');
+    
+    var QWeb = core.qweb;
+    
+    var FormView_Extend = FormView.include({
+        render_buttons: function($node) {
+            this.$buttons = $('<div/>');
+            
+            var $footer = this.$('footer');
+            if (this.options.action_buttons !== false || this.options.footer_to_buttons && $footer.children().length === 0) {
+                this.$buttons.append(QWeb.render("FormView.buttons", {'widget': this}));
+            }
+            if (this.options.footer_to_buttons) {
+                $footer.appendTo(this.$buttons);
+            }
+            
+            // Show or hide the buttons according to the view mode
+            this.toggle_buttons();
+            this.$buttons.on('click', '.o_form_button_create', this.on_button_create);
+            this.$buttons.on('click', '.o_form_button_edit', this.on_button_edit);
+            this.$buttons.on('click', '.o_form_button_save', this.on_button_save);
+            this.$buttons.on('click', '.o_form_button_cancel', this.on_button_cancel);
+            this.$buttons.on('click', '.o_form_button_popup', this.on_button_popup);
+            
+            this.$buttons.appendTo($node);
+        },
+        on_button_popup: function() {
+            var self = this;
+            
+            self.url_val = self.get_action_value('URL');
+            
+            if(!self.url_val)
+            {
+                $(".o_form_radio").each(function(index,item){
+                    if($(item).is( ":checked" ))
+                    {
+                        if($(item).val().startsWith("URL:"))
+                        {
+                            self.url_val = $(item).val().substr(4);
+                            return;
+                        }
+                    }
+                });
+            }
+            
+            if(self.url_val)
+                $.colorbox({href:self.url_val, iframe:true, width:900, height:600});
+            else
+                $.colorbox({html:'There is no popup option selected.<br/><br/>' + 
+                    
+                                'You should set a selection field in the model with the format URL:dns_address<br/>' + 
+                                'urls = fields.Selection(string="URLs",' + 
+                                'selection = [("URL:https://www.google.com", "https://www.google.com"),' + 
+                                '("URL:https://www.wikipedia.com", "https://www.wikipedia.com"),' + 
+                                '("URL:http://localhost:8069", "http://localhost:8069")])<br/><br/>' + 
+                                
+                                'And set the selection field with widget="radio" in the view:<br/>' + 
+                                '&lt;field name="urls" options="{\'horizontal\': true}" widget="radio"/&gt;<br/><br/>' + 
+                                
+                                'Or set the property URL="https://www.wikipedia.com" in the \'form\' tag:<br/>' + 
+                                '&lt;form string="Form title one" URL="https://www.wikipedia.com"&gt;<br/>' + 
+                                '    ...<br/>' + 
+                                '    ...<br/>' + 
+                                '&lt;form/&gt;'});
+        },
+        is_action_enabled: function(action) {
+            var attrs = this.fields_view.arch.attrs;
+            return (action in attrs) ? JSON.parse(attrs[action]) : false;
+            //return (action in attrs) ? JSON.parse(attrs[action]) : true;
+        },
+        get_action_value: function(action) {
+            var attrs = this.fields_view.arch.attrs;
+            return (action in attrs) ? attrs[action] : false;
+            //return (action in attrs) ? JSON.parse(attrs[action]) : true;
+        },
+    });
+    
+    return {
+        FormView: FormView_Extend,
+    };
+    
+});
