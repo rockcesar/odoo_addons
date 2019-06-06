450785a0dae2dbb3a0a1a960776bee4f88a8c1fb
César Cordero <rockcesar@debian-cesar.Home>
2019-06-06 10:53:55 -0400

    Translator Online for Odoo.

---
+++ translator_odoo/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{   'active': True,
+    'author': u'Lcdo. César Cordero Rodríguez, part of University of Zulia professionals',
+    'website': 'https://teaching.cr-innova-consultants.com',
+    'category': 'Translation',
+    'demo_xml': [],
+    'depends': [
+        'base',
+        ],
+    'description': '''
+        \n\nThis is a new translator.
+    ''',
+    'init_xml': [],
+    'installable': True,
+    'license': 'AGPL-3',
+    'name': u'Good stuffs',
+    'test': [],
+    'data': [
+        #'security/ir.model.access.csv',
+        'views/translator_views.xml',
+    ],
+    'version': '',
+    'application': True,
+}
