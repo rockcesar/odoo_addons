a6962cbc50d5fbacc16ed798e3e1efb6dacc3345
César Cordero <rockcesar@debian-cesar.Home>
2019-06-06 18:48:10 -0400

    Description updated.

--- translator_odoo/__manifest__.py
+++ translator_odoo/__manifest__.py
@@ -11,13 +11,21 @@
         This is a new translator.
 
         Libraries to install:
+
 	    This library py_translator:
+
                 https://pypi.org/project/py-translator/
+
 	    Download the library:
+
                 wget https://files.pythonhosted.org/packages/17/37/27d6e7fa49cf383d556e8470a4ec39133a66f19bc24823ce1e395ed93d38/py_translator-2.1.9.tar.gz
+
                 tar -xvf py_translator-2.1.9.tar.gz
+
                 cd py_translator-2.1.9
+
                 python3 setup.py install
+
 	    Install Tor:
                 aptitude install torsocks tor
     ''',
