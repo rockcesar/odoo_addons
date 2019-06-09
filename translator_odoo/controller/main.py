d98e888a03ac587c96bead67188185d2f3a092fc
César Cordero <rockcesar@debian-cesar.Home>
2019-06-09 08:17:02 -0400

    tranlator_odoo updated.

--- translator_odoo/controller/main.py
+++ translator_odoo/controller/main.py
@@ -31,15 +31,10 @@ class ControllerTranslate(http.Controller):
         values = kwargs
         
         if 'language' in values:
-            values.update({'name': 'Translation ' + str((datetime.now()).strftime(DEFAULT_SERVER_DATETIME_FORMAT))})
+            #values.update({'name': 'Translation ' + str((datetime.now()).strftime(DEFAULT_SERVER_DATETIME_FORMAT))})
             
-            if 'name_of_file' in values:
-                values['name_of_file'] = values['name_of_file'].read().decode("utf-8").replace('\\ n', '\n').replace('\\n', '\n')
-                
             if values['name'] == "":
                 del values['name']
-            if 'type_of_input' in values and values['type_of_input'] == "":
-                del values['type_of_input']
             if 'language'in values and values['language'] == "":
                 del values['language']
             if 'text' in values and values['text'] == "":
@@ -48,6 +43,11 @@ class ControllerTranslate(http.Controller):
                 del values['term_translated']
             if 'code' in values and values['code'] == "":
                 del values['code']
+            
+            if 'type_of_input' in values and (values['type_of_input'] == "" or values['type_of_input'] == "using_file"):
+                if 'name_of_file' in values:
+                    if type(values['name_of_file']) != str:
+                        values['name_of_file'] = values['name_of_file'].read().decode("utf-8").replace('\\ n', '\n').replace('\\n', '\n')
             
             language_id = request.env['language'].sudo(SUPERUSER_ID).search([('code', '=', values['language'])])
             values.update({'language': language_id.id})
@@ -66,5 +66,5 @@ class ControllerTranslate(http.Controller):
         values.update({'languages': request.env['language'].sudo(SUPERUSER_ID).search([])})
         values.update({'type_of_input_list': [{'code': 'using_text_field', 'name': 'Using Text Field'}, 
                                     {'code': 'using_file', 'name': 'Using File'}]})
-                                    
+
         return request.render("translator_odoo.translator_website", values)
