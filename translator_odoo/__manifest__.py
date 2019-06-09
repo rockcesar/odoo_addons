bc7cba77317020f7c302dfde45ec4844a3cf6bc0
César Cordero <rockcesar@debian-cesar.Home>
2019-06-09 01:13:53 -0400

    translator_odoo updated.

--- translator_odoo/__manifest__.py
+++ translator_odoo/__manifest__.py
@@ -1,24 +1,47 @@
 # -*- coding: utf-8 -*-
 {   'active': True,
-    'author': u'Lcdo. César Cordero Rodríguez, part of University of Zulia professionals',
+    'author': u'Lcdo. César Cordero Rodríguez, part of CR-Innova Consultants and University of Zulia professionals',
     'website': 'https://teaching.cr-innova-consultants.com',
     'category': 'Translation',
     'demo_xml': [],
     'depends': [
         'base',
-        ],
+    ],
     'description': '''
-        \n\nThis is a new translator.
+        This is a new translator.
+
+        Libraries to install:
+
+	    This library py_translator:
+
+                https://pypi.org/project/py-translator/
+
+	    Download the library:
+
+                wget https://files.pythonhosted.org/packages/17/37/27d6e7fa49cf383d556e8470a4ec39133a66f19bc24823ce1e395ed93d38/py_translator-2.1.9.tar.gz
+
+                tar -xvf py_translator-2.1.9.tar.gz
+
+                cd py_translator-2.1.9
+
+                python3 setup.py install
+
+	    Install Tor:
+                aptitude install torsocks tor
     ''',
     'init_xml': [],
     'installable': True,
     'license': 'AGPL-3',
-    'name': u'Good stuffs',
+    'name': u'Translator for Odoo',
     'test': [],
     'data': [
-        #'security/ir.model.access.csv',
+        'security/translator_odoo_security.xml',
+        'security/ir.model.access.csv',
         'views/translator_views.xml',
+        'views/languages_views.xml',
+        'views/translator_templates.xml',
     ],
+    
     'version': '',
     'application': True,
 }
--- translator_odoo/models/translator.py
+++ translator_odoo/models/translator.py
@@ -10,20 +10,30 @@ import sys
 
 import base64
 
+class Languages(models.Model):
+    _name = "language"
+    _description = "Languages"
+    _order = "name asc"
+
+    name = fields.Char("Language name")
+    code = fields.Char("Language code")
+    uses = fields.Integer("Number of uses")
+
 class Translator_online(models.Model):
     _name = "translator"
+    _description = "Translator"
 
-    name = fields.Char("Name of term to translate")
+    name = fields.Char("Your name")
+    email = fields.Char("Your email")
     text = fields.Text("Text to be translated")
     term_translated = fields.Text("Term translated")
     name_of_file = fields.Binary("File to translate")
-    language = fields.Char("Language to translate")
-    type_of_input = fields.Char("Type of input")
+    language = fields.Many2one("language", string="Languages to translate")
+    #language = fields.Selection([('es', 'spanish'), ('en', 'english'), ('af', 'afrikaans'), ('am', 'amharic'), ('ar', 'arabic'), ('az', 'azerbaijani'), ('be', 'belarusian'), ('bg', 'bulgarian'), ('bn', 'bengali'), ('bs', 'bosnian'), ('ca', 'catalan'), ('ceb', 'cebuano'), ('co', 'corsican'), ('cs', 'czech'), ('cy', 'welsh'), ('da', 'danish'), ('de', 'german'), ('el', 'greek'), ('eo', 'esperanto'), ('et', 'estonian'), ('eu', 'basque'), ('fa', 'persian'), ('fi', 'finnish'), ('fil', 'Filipino'), ('fr', 'french'), ('fy', 'frisian'), ('ga', 'irish'), ('gd', 'scots gaelic'), ('gl', 'galician'), ('gu', 'gujarati'), ('ha', 'hausa'), ('haw', 'hawaiian'), ('he', 'Hebrew'), ('hi', 'hindi'), ('hmn', 'hmong'), ('hr', 'croatian'), ('ht', 'haitian creole'), ('hu', 'hungarian'), ('hy', 'armenian'), ('id', 'indonesian'), ('ig', 'igbo'), ('is', 'icelandic'), ('it', 'italian'), ('iw', 'hebrew'), ('ja', 'japanese'), ('jw', 'javanese'), ('ka', 'georgian'), ('kk', 'kazakh'), ('km', 'khmer'), ('kn', 'kannada'), ('ko', 'korean'), ('ku', 'kurdish (kurmanji)'), ('ky', 'kyrgyz'), ('la', 'latin'), ('lb', 'luxembourgish'), ('lo', 'lao'), ('lt', 'lithuanian'), ('lv', 'latvian'), ('mg', 'malagasy'), ('mi', 'maori'), ('mk', 'macedonian'), ('ml', 'malayalam'), ('mn', 'mongolian'), ('mr', 'marathi'), ('ms', 'malay'), ('mt', 'maltese'), ('my', 'myanmar (burmese)'), ('ne', 'nepali'), ('nl', 'dutch'), ('no', 'norwegian'), ('ny', 'chichewa'), ('pa', 'punjabi'), ('pl', 'polish'), ('ps', 'pashto'), ('pt', 'portuguese'), ('ro', 'romanian'), ('ru', 'russian'), ('sd', 'sindhi'), ('si', 'sinhala'), ('sk', 'slovak'), ('sl', 'slovenian'), ('sm', 'samoan'), ('sn', 'shona'), ('so', 'somali'), ('sq', 'albanian'), ('sr', 'serbian'), ('st', 'sesotho'), ('su', 'sundanese'), ('sv', 'swedish'), ('sw', 'swahili'), ('ta', 'tamil'), ('te', 'telugu'), ('tg', 'tajik'), ('th', 'thai'), ('tl', 'filipino'), ('tr', 'turkish'), ('uk', 'ukrainian'), ('ur', 'urdu'), ('uz', 'uzbek'), ('vi', 'vietnamese'), ('xh', 'xhosa'), ('yi', 'yiddish'), ('yo', 'yoruba'), ('zh-cn', 'chinese (simplified)'), ('zh-tw', 'chinese (traditional)'), ('zu', 'zulu')], "Language to translate")
+    type_of_input = fields.Selection([('using_text_field', 'Using Text Field'), ('using_file', 'Using File')], "Type of input")
 
     @api.one
     def translate(self):
-        #_logger.info("1234567890")
-
         proxy = {
             'socks': 'socks://localhost:9050',
             'socks': 'socks://localhost:9050',
@@ -36,13 +46,34 @@ class Translator_online(models.Model):
         text = ""
 
         if self.type_of_input == "using_file":
-            #translator_file = base64.decode(self.name_of_file)
             text = str(base64.b64decode(self.name_of_file).decode("utf-8")).replace('\\ n', '\n').replace('\\n', '\n')
-            #file = open(translator_file, "r")
-            #text = file.read()
         else:
             text = self.text
 
-        s = Translator(proxies=proxy).translate(text=text, dest=self.language).text
+        s = Translator(proxies=proxy).translate(text=text, dest=self.language.code).text
 
         self.term_translated = s
+        
+    @api.one
+    def translate_to_website(self):
+        proxy = {
+            'socks': 'socks://localhost:9050',
+            'socks': 'socks://localhost:9050',
+        }
+
+        ROTATING_PROXY_LIST = {
+            'socks': 'socks://localhost:9050'
+        }
+
+        text = ""
+
+        if self.type_of_input == "using_file":
+            text = self.name_of_file.decode("utf-8").replace('\\ n', '\n').replace('\\n', '\n')
+        else:
+            text = self.text
+
+        s = Translator(proxies=proxy).translate(text=text, dest=self.language.code).text
+
+        self.term_translated = s
+        
+        return self.term_translated
