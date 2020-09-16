42781561646ec118a03e13ae98a496b426c52f38
César Cordero Rodríguez <cesar.cordero.r@gmail.com>
2020-09-16 15:06:40 -0400

    Update __manifest__.py

--- website_pinetwork_odoo/__manifest__.py
+++ website_pinetwork_odoo/__manifest__.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
 {
     'name': 'PiNetwork Hello World for Odoo',
     'category': 'Website/Website',
     'sequence': 54,
     'summary': 'PiNetwork Hello World',
     'version': '2.1',
+    "author": "César Cordero Rodríguez <cesar.cordero.r@gmail.com>",
+    "website": "https://cr-innova.negocio.site/",
+    "contributors": [
+        "César Cordero Rodríguez <cesar.cordero.r@gmail.com>",
+    ],
     'description': """
 PiNetwork Hello World for Odoo
 -----------------
