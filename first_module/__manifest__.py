903d6004ff71c10dcc2f50b730ec7261db84b312
César Cordero Rodríguez <cesar.cordero.r@gmail.com>
2022-04-16 17:03:24 -0400

    Update __manifest__.py

--- first_module/__manifest__.py
+++ first_module/__manifest__.py
@@ -3,11 +3,7 @@
 
 {
     'name': 'First module (for learning Odoo)',
-<<<<<<< HEAD
     'depends': ['base', 'web', 'website'],
-=======
-    'depends': ['base', 'web', 'website', 'point_of_sale'],
->>>>>>> 961b5f77435db53686f600f11b302b3f8d9aa107
     'author': 'César Cordero Rodríguez <cesar.cordero.r@gmail.com>',
     'website': 'https://cr-innova.negocio.site/',
     'contributors': [
@@ -34,11 +30,7 @@
         'views/menus_v.xml',
         'security/ir.model.access.csv',
     ],
-<<<<<<< HEAD
     'qweb': [
-=======
-    'qweb': [ 'static/src/xml/OrderReceipt.xml'
->>>>>>> 961b5f77435db53686f600f11b302b3f8d9aa107
     ],
     "installable": True,
 }
