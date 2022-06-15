7203c328ec22c1b1dd4f6d02206555cfc471cab8
César Cordero Rodríguez <cesar.cordero.r@gmail.com>
2022-06-15 04:04:58 -0400

    Update app.js

--- website_pinetwork_games_odoo/static/src/pidoku/assets/js/app.js
+++ website_pinetwork_games_odoo/static/src/pidoku/assets/js/app.js
@@ -145,7 +145,7 @@ var refresh_board = function(){
                          "import"];
 
         for(var i = 0; i < 7; i++){
-            $("#"+tabs_names[i]).attr("disabled", false);
+            $("#"+tabs_names[i]).addClass("disabled");
         }
     }
 }
