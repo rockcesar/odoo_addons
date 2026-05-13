cdda0b2e04df6c70a9062fbdfd6de4cac373b45e
Cesar Cordero <cesar.cordero.r@gmail.com>
2026-05-13 17:35:02 -0400

    Upgrade

--- payment_pi_network/__manifest__.py
+++ payment_pi_network/__manifest__.py
@@ -12,6 +12,10 @@
                        
                        Repo:
                        https://github.com/rockcesar/odoo_addons/tree/18.0
+                       
+                       Make sure to create the Pi currency in Odoo to make works the following line:
+                       https://github.com/rockcesar/odoo_addons/blob/e7c5efd6004c52ff15abaeac9a0ebc3c3d20d749/payment_pi_network/models/payment_provider.py#L25
+                       res |= self.env['res.currency'].search([('name', '=', 'PI')])
                    """,
     'version': '1.0',
     "author": "César Cordero Rodríguez <cesar.cordero.r@gmail.com>",
